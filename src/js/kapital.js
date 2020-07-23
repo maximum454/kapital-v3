1164f68f762ad61f87ee4ff1874edec928b65696
maxim <maximum454@gmail.com>
2020-07-23 12:16:09 +0600

    Цитата дня, лента новостей, слайдер спец проектов

--- src/js/kapital.js
+++ src/js/kapital.js
@@ -1,55 +1,24 @@
 @@include('partials/jquery.min.js')
 @@include('partials/webp.js')
-@@include('partials/burger.js')
+@@include('partials/priority-nav.js')
 @@include('partials/jquery.knob.js')
 @@include('partials/slick.js')
 
 $(function(){
-    $nav = $('.header');
-    $upline = $('.upline');
-    $window = $(window);
-    $h = $nav.offset().top;
-    $window.scroll(function(){
-        if ($window.scrollTop() > $h) {
-            $nav.addClass('fixed');
-            $upline.addClass('active');
-        } else {
-            $nav.removeClass('fixed');
-            $upline.removeClass('active');
-        }
+    var nav = priorityNav.init({
+        mainNavWrapper: ".nav__wrapper",
+        mainNav: ".nav__menu",
+        navDropdownLabel: '...',
+        navDropdownClassName: "nav__dropdown",
+        navDropdownToggleClassName: "nav__dropdown-toggle",
     });
-});
 
-
-
-$('.js-partners-slider').slick({
-    centerMode: false,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: false,
-    infinite: true,
-    prevArrow: $('.partners__prev'),
-    nextArrow: $('.partners__next'),
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                centerPadding: '40px',
-                slidesToShow: 3
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 2
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1
-            }
-        }
-    ]
+    $('.js-special-slider').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    })
 });
